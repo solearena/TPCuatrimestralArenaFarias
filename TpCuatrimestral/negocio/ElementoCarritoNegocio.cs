12a1e2a93ef623601e62fdaaa7eb97f747e7743a
Florencia Farias <fariasflorencia@hotmail.com>
2021-10-29 19:52:57 -0300

    Se agregan las cnx a la BBDD

---
+++ TpCuatrimestral/negocio/ElementoCarritoNegocio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using dominio;
+
+namespace negocio
+{
+    public class ElementoCarritoNegocio
+    {
+        public List<ElementoCarrito> listar()
+        {
+            List<ElementoCarrito> lista = new List<ElementoCarrito>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("");//agregar consulta de bbdd
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    ElementoCarrito aux = new ElementoCarrito();
+                    /*aux.Id = (int)datos.Lector["Id"];
+                    aux.CodigoArticulo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+                    if (!(datos.Lector["ImagenUrl"] is DBNull))
+                    {
+                        aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    }
+                    aux.DescripcionMarca = new Marca();
+                    aux.DescripcionMarca.Id = (int)datos.Lector["Id"];
+                    aux.DescripcionMarca.Descripcion = (string)datos.Lector["Marca"];
+                    aux.DescripcionCategoria = new Categoria();
+                    aux.DescripcionCategoria.Id = (int)datos.Lector["Id"];
+                    aux.DescripcionCategoria.Descripcion = (string)datos.Lector["Categoria"];
+                    lista.Add(aux);*/
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ TpCuatrimestral/negocio/VentaNegocio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using dominio;
+
+ namespace negocio
+{
+    public class VentaNegocio
+    {
+        public List<Venta> listar()
+        {
+            List<Venta> lista = new List<Venta>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("");//agregar consulta de bbdd
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Venta aux = new Venta();
+                    /*aux.Id = (int)datos.Lector["Id"];
+                    aux.CodigoArticulo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+                    if (!(datos.Lector["ImagenUrl"] is DBNull))
+                    {
+                        aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    }
+                    aux.DescripcionMarca = new Marca();
+                    aux.DescripcionMarca.Id = (int)datos.Lector["Id"];
+                    aux.DescripcionMarca.Descripcion = (string)datos.Lector["Marca"];
+                    aux.DescripcionCategoria = new Categoria();
+                    aux.DescripcionCategoria.Id = (int)datos.Lector["Id"];
+                    aux.DescripcionCategoria.Descripcion = (string)datos.Lector["Categoria"];
+                    lista.Add(aux);*/
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
